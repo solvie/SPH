254f3f41855740de7db50ffd043310bc1f40367c
Solvie Lee <solvie.lee@mail.mcgill.ca>
2018-01-25 17:58:33 -0500

    Syndrome added

--- errorchannel/syndromedecode844.m
+++ errorchannel/syndromedecode844.m
@@ -1,4 +1,4 @@
-stepsize=0.02;
+stepsize=0.05;
 lastp = 1;
 random = 0.5;
 
@@ -11,38 +11,55 @@ for i = 0:(2^4-1)
    codebook(i+1,:) = mod(de2bi(i,4,'left-msb')*G,2);%generate codebook entry
 end
 
+%Generator Matrix G = [1 0 0 0 1 1 1 0;0 1 0 0 1 1 0 1; 0 0 1 0 1 0 1 1; 0 0 0 1 0 1 1 1];
+G= [eye(4),rot90(~eye(4))];
+% Parity Check Matrix H
+H= [rot90(~eye(4)),eye(4)];
+
+
+%Generate the syndrome table error side
+E = [zeros(1,8);eye(8,8)];
+
+%Generate the syndrome table syndrome side
+Syn= zeros(1,4);
+for i = 2: length(E)
+    s = mod(H*E(i,:)',2);
+    Syn=[Syn;s'];
+end
+
+
 codebook = logical(codebook);
 
 for j = 0: numRows % for each p
     p = double(j)*stepsize;
     errorSum = 0;
     numTrials = 50000;
+    index =10;
     for k = 1:numTrials % take average over 10 values, arbitrarily for now
         %Generate random message
         m = rand(1,4) < random;
-        %Generator Matrix G = [1 0 0 0 1 1 1 0;0 1 0 0 1 1 0 1; 0 0 1 0 1 0 1 1; 0 0 0 1 0 1 1 1];
-        G= [eye(4),rot90(~eye(4))];
-        
         %Bit Error function from channel, p hardcoded to 0.2
         e = rand(1,8) < p;
         %Codeword created from message and Generator 
         c = mod (m*G,2);
         %Corrupt part of the codeword using bit error channel
-        y = mod (c+e,2);
-
-        chat = [];
-        v = 9; % Set initial hamming distance to arbitrary max greater than 8
-
-        for z = 1:(2^4)
-          if v > sum(abs(y-codebook(z,:))) %found a codeword with a lower hamming distance
-              v = sum(abs(y-codebook(z,:)));
-              chat = codebook(z,:);
-          end
+        r = mod (c+e,2);
+       
+        Hrt =mod( H*r',2);
+        for i = 1: length(Syn)
+           if max(abs(Hrt'-Syn(i,:)))==0 
+              index = i;
+              break
+           end
         end
-        
-        mhat = chat(1:4);
-        %find number of different bits between mhat and m 
-        numDiffs = sum(abs(mhat-m));
+        if index==10 % if not found, max error
+            numDiffs=4;
+        else 
+            ehat = E(index,:);
+            chat = mod(ehat+r,2);
+            mhat = chat(1:4);
+            numDiffs = sum(abs(mhat-m));
+        end
         errorSum = errorSum+numDiffs;
     end
     errorAvg = (errorSum/numTrials)/length(m);
@@ -50,6 +67,6 @@ for j = 0: numRows % for each p
 end 
 
 plot(ResultMatrix(:,1),ResultMatrix(:,2));
-title('BER For Channel Exhaustive Decode ');
+title('BER For Channel Syndrome Decode ');
 xlabel('P_{error}');
 ylabel('Error Rate');
