6c27bb3bed99e79a467309e438a66c266beaced1
Solvie Lee <solvie.lee@mail.mcgill.ca>
2018-02-02 13:56:38 -0500

    cleanup

--- erasurechannel/testencodedecode844.m
+++ erasurechannel/testencodedecode844.m
@@ -1,31 +1,27 @@
 function [ retval ] = testencodedecode844( p_error, codebook, print_msgs )
-    %Generate random message
-    retval = false;
-    m = rand(1,4) < 0.5;
-   % disp('Random message generated m:');
-    %disp(m);
-    
-    %Encode Codeword: m -> c
-    c = linearblockencoder844(m);
-  %  disp('Encoded message c:');
-   % disp(c);
-    
+   %Generate random message
+   m = rand(1,4) < 0.5;
+   %Encode Codeword: m -> c
+   c = linearblockencoder844(m);
    %Erase part of message with set probability using erasure channel: c -> y
-    y = erasurechannel(p_error,c);
-   % fprintf('Codeword passed through erasure channel with p_error = %d y:\n', p_error);
-   % disp(y);
-    
+   y = erasurechannel(p_error,c);
    %Decode received message using the codebook
-    decoded = decodemsglinblock844(codebook, y);
-   % disp('Decoded codeword mhat');
-   % disp(decoded);
-    
+   decoded = decodemsglinblock844(codebook, y);
+   
+   if print_msgs
+       disp('Random message generated m:');
+       disp(m);
+       disp('Encoded message c:');
+       disp(c);
+       fprintf('Codeword passed through erasure channel with p_error = %d y:\n', p_error);
+       disp(y);
+       disp('Decoded codeword mhat');
+       disp(decoded);
+   end
    if m==decoded
-        %disp('Message successfully decoded');
         retval = true;
-    else
-        %disp('Message not recovered');
+   else
         retval = false;
-    end
+   end
 end
 
