a6bdea8fd81f574e81e01f6223d09754b25ca1f9
Ankita Sharma <ankita.sharma2@mail.mcgill.ca>
2018-02-07 21:06:22 -0500

    NOTHING WORKS IN THIS. TO DO TONIGHT

--- erasurechannel/gaussiandecode844.m
+++ erasurechannel/gaussiandecode844.m
@@ -8,13 +8,14 @@ function [ c ] = gaussiandecode844( message )
            m(i)=0;
        end
     end
-   eqn1= m(5)== m(1)+m(2)+m(3);
-   eqn2= m(6)== m(1)+m(2)+m(4);
-   eqn3= m(7)== m(1)+m(3)+m(4);
-   eqn4= m(8)== m(2)+m(3)+m(4);
+   eqn1= m(5)== xor(xor(m(1),m(2)),m(3));
+   eqn2= m(6)== xor(xor(m(1),m(2)),m(4));
+   eqn3= m(7)== xor(xor(m(1),m(3)),m(4));
+   eqn4= m(8)== xor(xor(m(2),m(3)),m(4));
    
    X(1) = mod(solve(eqn1), 2);
    X(2) = mod(solve(eqn2), 2);
-   X(3) = solve(eqn3);
-   X(4) = solve(eqn4);
+   X(3) = mod(solve(eqn3), 2);
+   X(4) = mod(solve(eqn4), 2);
+   
    end
