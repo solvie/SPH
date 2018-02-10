9223c3e9e1e813708d562de0d37ca3a75a4e0b35
Solvie <lee.solvie@gmail.com>
2018-02-10 12:23:54 -0500

    Another cleanup

--- errorchannel/errorchannel.m
+++ errorchannel/errorchannel.m
@@ -1,6 +1,5 @@
 function [y] = errorchannel(p_error,codeword)
-%ERRORCHANNEL Summary of this function goes here
-    %Bit Error function from channel, p hardcoded to 0.2
+    %Bit Error function from channel
     e = rand(1,8) < p_error;
     %Corrupt part of the codeword using bit error channel
     y = mod (codeword+e,2);
