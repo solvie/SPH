3bf6f4dc7e80d70ddb040fce5669ca211b8a5bff
Solvie Lee <solvie.lee@mail.mcgill.ca>
2018-02-15 14:09:22 -0500

    temp created and organized into lab1 lab2

---
+++ lab2/temp.m
@@ -0,0 +1,16 @@
+m =[1 0 0 0];
+sr= zeros(1,4);
+padded_msg_len= length(sr)+length(m);
+c= zeros(1,padded_msg_len*2);
+m = [m ,sr];
+%encoder
+i= 1;
+while i< length(c)
+    c(i)= mod(m(int8(i/2))+sr(3)+sr(4),2);
+    c(i+1)= mod(m(int8(i/2))+sr(2)+sr(3)+sr(4),2);
+    sr(4)= sr(3);
+    sr(3)= sr(2);
+    sr(2)= sr(1);
+    sr(1)= m(int8(i/2));
+    i = i+2;
+end
